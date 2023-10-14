aad0973c7e20c34574eefdce7b0f98a14e47861d
gv68682 <gv68682@gmail.com>
2023-10-14 12:10:10 -0500

    stackheap

---
+++ stackheap.c
@@ -0,0 +1,16 @@
+//C++
+
+
+// #include <iostream>
+// #include <string>
+
+// int main(){
+//stack allocation
+//     int value =5; 
+//     int array[5];
+
+//Heap allocation
+//     int* hvalue = new int;
+//     *hvalue = 6;
+//     int* harray = new int[6];
+// }
